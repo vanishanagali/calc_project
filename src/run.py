5701ea5bf65168f0561020346417543901388c5a
Vanisha <vanishanagali@gmail.com>
2023-07-28 12:02:45 -0400

    moved demo.py to run.py

--- src/run.py
+++ src/run.py
@@ -1,3 +1,4 @@
+import time
 import keyboard 
 
 def add(x: float, y: float) -> float:
@@ -72,100 +73,99 @@ def divide(x: float, y: float) -> float:
     """
     return x / y
 
-# variable definitions
+def keyPress(x: str) -> bool:
+    key = keyboard.isPressed(x)
+    return key
+
+i = 0
+operations = ['[add] sub mul div', 'add [sub] mul div', 'add sub [mul] div', 'add sub mul [div]']
+arrayValue = '0'
 prevCalc = 'no'
-num1 = 0
-num2 = 0
-choice = 'none'
-addVar = '[add]'
-subVar = 'sub'
-mulVar = 'mul'
-divVar = 'div'
-addArr = ['\r[add] ', '\radd ']
-subArr = ['[sub] ', 'sub ']
-mulArr = ['[mul] ', 'mul ']
-divArr = ['[div]', 'div ']
-arrayValue = 1
+num_1 = 0
+num_2 = 0
+currKeyR = False
+currKeyL = False
+currKeyE = False
 
 # general start up
-def optionPicker(w, x, y, z):
-    print('Which operation?')
-    print(w, end='')
-    print(x, end='')
-    print(y, end='')
-    print(z, end='')
-    
-
-# while (arrayValue == 0):
-# optionPicker(addArr[0], subArr[1], mulArr[1], divArr[1])
-# arrayValue = 1
-# if arrayValue <= 4:
-#     if keyboard.is_pressed('right'):
-#         optionPicker(addArr[1], subArr[0], mulArr[1], divArr[1])
-#         arrayValue += 1
-#         if keyboard.is_pressed('right'):
-#             optionPicker(addArr[1], subArr[1], mulArr[0], divArr[1])
-#             arrayValue += 1
-#             if keyboard.is_pressed('right'):
-#                 optionPicker(addArr[1], subArr[1], mulArr[1], divArr[0])
-#                 arrayValue += 1
-# else: 
-#     arrayValue = 1
-    
-        # input("hi: ")
-# elif arrayValue > 2: 
-    # elif keyboard.is_pressed('right')
-
-
-
 while True:
-    # input operation
-   # options()
-    choice = input('Choose which operation (1/2/3/4):')
+    time.sleep(0.5)
+    while arrayValue == '0':
+        print("Use your arrow keys and enter to select operation: ")
 
-    # input validation & input numbers
-    if choice not in ('1', '2', '3', '4'):
-        print('Invalid input. Restart.')
-        break
+        # interface to choose operation
+        while i <= 3:
+            print(f'\r{operations[i]}', end="")
+            prevKeyR = currKeyR
+            currKeyR = keyboard.is_pressed('right')
+            prevKeyL = currKeyL
+            currKeyL = keyboard.is_pressed('left')
+            prevKeyE = currKeyE
+            currKeyE = keyboard.is_pressed('enter')
+            # increment and decrement depending on key press 
+            if  prevKeyR == False and currKeyR == True:
+                print(f'\r{operations[i]}', end="")
+                i += 1
+            if prevKeyL == False and currKeyL == True:
+                print(f'\r{operations[i]}', end="")
+                i -= 1
+            if prevKeyE == False and currKeyE == True:
+                time.sleep(0.5)
+                arrayValue = operations[i]
+                i = 5
+                
+        # set operation interface to intial
+        while i == 4:
+            print(f'\r{operations[0]}', end="")
+            i = 0
+        
+        # input validation & input numbers
+        while i == 5:
+            key1 = False
+            key2 = False
+            key3 = False
+            time.sleep(2)
+            if prevCalc == 'no' or prevCalc == 'n':
+                try:
+                    num_1 = float(input("\nFirst number: "))
+                except ValueError:
+                    print('Invalid. Restart.')
+                    break
+            elif prevCalc == 'yes' or prevCalc == 'y':
+                num_1 = answer
+            try:
+                num_2 = float(input('Second number: '))            
+            except ValueError:
+                print('Invalid. Restart.')
+                break
 
-    if prevCalc == 'no' or prevCalc == 'n':
-        try:
-            num1 = float(input('First number: '))
-        except ValueError:
-            print('Invalid. Restart.')
-            break
-    elif prevCalc == 'yes' or prevCalc == 'y':
-        num1 = answer
-    try:
-        num2 = float(input('Second number: '))
-    except ValueError:
-        print('Invalid. Restart.')
-        break
-   
-    # perform calculation
-    if choice == '1':
-        answer = add(num1, num2)
-    elif choice == '2':
-        answer = subtract(num1, num2)
-    elif choice == '3':
-        answer = multiply(num1, num2)
-    elif choice == '4':
-        if num2 == 0:
-            print('Cannot divide by 0. Error.')
-            break
-        answer = divide(num1, num2)
+            # perform calculation
+            if arrayValue == operations[0]:
+                answer = add(num_1, num_2)
+            elif arrayValue == operations[1]:
+                answer = subtract(num_1, num_2)
+            elif arrayValue == operations[2]:
+                answer = multiply(num_1, num_2)
+            elif arrayValue == operations[3]:
+                if num_2 == 0:
+                    print('Cannot divide by 0. Error.')
+                    break
+                answer = divide(num_1, num_2)
 
-    print('Your answer is ', answer)
+            print('Your answer is ', answer)
 
-    # another calc?
-    nextCalc = input('Another calc? (yes/no)')
-    if nextCalc == 'no' or nextCalc == 'n':
-        break
-    prevCalc = input('Would you like to use the previous answer? (yes/no)')
+            # another calc?
+            nextCalc = input('Another calc? (yes/no)')
+            if nextCalc == 'no' or nextCalc == 'n':
+                break
+            prevCalc = input('Would you like to use the previous answer? (yes/no)')
 
-'''
-while True:
-    if keyboard.is_pressed('right'):
-        print("Dear John")
-        break
-'''
+    # print(f'\r{i}', end="")
+    # print(f'\r{options[i]}', end="")
+    # time.sleep(2)
+    # print(f'\r{options[i]}', end="")
+    # time.sleep(2)
+    # print(f'\r{options[i]}', end="")
+    # time.sleep(2)
+    # print(f'\r{options[i]}', end="")
+    # time.sleep(2)
